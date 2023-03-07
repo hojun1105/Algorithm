5b18a134b602171049213180387c92f25c6e3d4e
Hojun <75318501+hojun1105@users.noreply.github.com>
2023-03-07 22:11:56 +0900

    Time: 134 ms (24.83%), Space: 50.8 MB (71.91%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        return Array.IndexOf(nums, target);
+        if(!nums.Contains(target))
+            return -1;
+        
+        for(int a=0; a<nums.Length; a++)
+        {
+            if(nums[a]==target)
+                return a;
+            
+        }
+        return -1;
     }
 }
