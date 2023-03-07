6057e7f9f23ef5b3b47dbb93ae8d08851b166b99
Hojun <75318501+hojun1105@users.noreply.github.com>
2023-03-07 22:16:10 +0900

    Time: 124 ms (63.94%), Space: 51 MB (59.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -2,13 +2,12 @@ public class Solution {
     public int Search(int[] nums, int target) {
         if(!nums.Contains(target))
             return -1;
-        
+        var c =-1;
         for(int a=0; a<nums.Length; a++)
         {
             if(nums[a]==target)
-                return a;
-            
+                c=a;
         }
-        return -1;
+        return c;
     }
 }
