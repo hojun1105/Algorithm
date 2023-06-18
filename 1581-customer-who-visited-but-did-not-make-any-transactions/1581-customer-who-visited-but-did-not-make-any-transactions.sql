e7b068f17e4c57eb22201ac8af4e13466d8ede68
Hojun <75318501+hojun1105@users.noreply.github.com>
2023-06-18 17:50:55 +0900

    Time: 2771 ms (17.92%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -6,5 +6,5 @@ select customer_id, count(*) as count_no_trans from
 (
     Visits a left join Transactions b
     on a.visit_id = b.visit_id 
-) where b.visit_id is null
-group by customer_id having(customer_id)>0
+) where amount is null
+group by customer_id
