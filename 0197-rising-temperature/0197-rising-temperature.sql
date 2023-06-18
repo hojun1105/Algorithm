7cc6987228a2fddf9a3af22db34c3c5d24c5ec50
Hojun <75318501+hojun1105@users.noreply.github.com>
2023-06-18 18:58:26 +0900

    Time: 842 ms (49.51%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,7 @@
 /* Write your T-SQL query statement below */
 
-select id from 
+select id 
+from 
 (Weather a inner join (select CONVERT(CHAR(10), DateAdd(dd, +1, recordDate), 23) as recordDate , temperature as yesterDayTemperature from Weather) b
 on a.recordDate = b.recordDate)
 where temperature > yesterDayTemperature
