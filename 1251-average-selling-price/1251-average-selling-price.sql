fe5ede8fe49a1226bada295ef2ed25507ee83453
Hojun <75318501+hojun1105@users.noreply.github.com>
2024-03-03 16:25:05 +0900

    Time: 1535 ms (7.90%), Space: 0B (100.00%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+select p.product_id as product_id, 
+CASE 
+    WHEN SUM(ISNULL(u.units, 0)) = 0 THEN 0
+    ELSE ROUND(CONVERT(FLOAT, SUM(p.price * ISNULL(u.units, 0))) / NULLIF(SUM(ISNULL(u.units, 0)), 0), 2)
+  END AS average_price
+from Prices p 
+left outer join UnitsSold u 
+on p.product_id = u.product_id 
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
